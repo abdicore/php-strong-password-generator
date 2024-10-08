af21dd6cdc457e17838db2e703f053be5b0798f9
abdicore <abdirhamanmohamed@gmail.com>
2024-10-08 09:28:49 +0200

    milestone2

--- functions/function.php
+++ functions/function.php
@@ -8,11 +8,17 @@ $numberLenght = $_GET["numberUser"];
 }
     
 function generatePassword($passwordLength){
-    $letters = 'abcdefghilmnopqrstvz0123456789@ABCDILMNEOPQRSTVZ!"#$%&()*+,-./:;=?@[\]^_{|}~';
+    $letters = 'abcdefghilmnopqrstvz0123456789';
+    $capLetters = 'ABCDILMNEOPQRSTVZ';
+    $numbers = '0123456789';
     $password = '';
-    for($i = 0; $i < $passwordLength; $i++){
+    for($i = 0; $i < ($passwordLength / 3); $i++){
         $selectedCharacter = $letters[rand(0,strlen($letters)-1)];
-        $password .= $selectedCharacter;
+        $selectedCapCharacter = $capLetters[rand(0,strlen($capLetters)-1)];
+        $selectedNumbers = $capLetters[rand(0,strlen($numbers)-1)];
+
+
+        $password .= $selectedCharacter.$selectedCapCharacter.$selectedNumbers;
     }
     return $password;
 }
